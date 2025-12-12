e5f5e973c6226b88865d2d4ce2a9b5f490198f15
MiguelSantos1910 <miguelsantos51346@gmail.com>
2025-12-11 21:31:36 -0300

    Update API base URL to include '/api'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL:'https://monitoramento-maquinas-backend-corrigido.onrender.com';
+    baseURL:'https://monitoramento-maquinas-backend-corrigido.onrender.com/api';
 });
 
 export default api;
+
 
